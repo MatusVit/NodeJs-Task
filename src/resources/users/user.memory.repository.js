fc5e1c1ea1ecf42e657c4d010b80e586486ec3f6
Vitali Matusevich <vimatu@gmail.com>
2020-10-11 23:30:43 +0300

    feat: add GET/users, GET/users:id, POST/users

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,12 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const DB = require('../../common/inMemoryDb');
+
+const getAll = async () => DB;
+
+const get = async id => DB.filter(el => el.id === id)[0];
+
+const create = async user => {
+  DB.push(user);
+  return get(user.id);
 };
 
-module.exports = { getAll };
+module.exports = { getAll, get, create };
